123ab5038963bedf061ecfd17c4506e86865d43c
hiper2d <hiper2d@gmail.com>
2022-08-05 21:45:34 -0400

    Update README.md with Double division

--- src/main/kotlin/com/hiper2d/tmp/CoffeeMachine.kt
+++ src/main/kotlin/com/hiper2d/tmp/CoffeeMachine.kt
@@ -1,63 +1,18 @@
-package com.hiper2d.tmp
+abstract class Coffee
+class Latte: Coffee()
+class Americano: Coffee()
+enum class CoffeeType {
+    LATTE, AMERICANO
+}
 
 class CoffeeMachine {
-    private var beans: Int = 0
-    private var watter: Int = 0
-    private var milk: Int = 0
-    private var sugar: Int = 0
+    private var beansLevel = 0
+    private var milkLevel = 0
 
-    init {
-        addSupplies()
-    }
-
-    @Synchronized
-    fun makeCoffee(type: CoffeeType, roast: CoffeeRoast): Coffee {
-        if (checkSupplies(type)) {
-            substractSupplies(type)
-            return when (type) {
-                CoffeeType.LATE -> Late(roast)
-                CoffeeType.CAPPUCCINO -> Cappuccino(roast)
-                CoffeeType.AMERICANO -> Americano(roast)
-            }
-        } else {
-            throw NotEnoughSuppliesException()
+    fun makeCoffee(coffeeType: CoffeeType): Coffee {
+        return when (coffeeType) {
+            CoffeeType.LATTE -> Latte()
+            CoffeeType.AMERICANO -> Americano()
         }
     }
-
-    private fun substractSupplies(type: CoffeeType) {
-        beans -= type.beans
-    }
-
-    private fun checkSupplies(type: CoffeeType) = type.water <= watter && type.beans <= beans && type.milk > milk
-
-    private fun addSupplies() {
-        beans = 100
-        watter = 100
-        milk = 100
-    }
-}
-
-class NotEnoughSuppliesException : RuntimeException() {
-
-}
-
-class UnknownCoffeeTypeException : RuntimeException() {
-
-}
-
-abstract class Coffee(val roast: CoffeeRoast) {
-}
-
-class Late(roast: CoffeeRoast): Coffee(roast)
-class Cappuccino(roast: CoffeeRoast): Coffee(roast)
-class Americano(roast: CoffeeRoast): Coffee(roast)
-
-enum class CoffeeType(val beans: Int, val milk: Int, val water: Int) {
-    LATE(beans = 1, milk = 5, water = 2),
-    CAPPUCCINO(beans = 1, milk = 5, water = 2),
-    AMERICANO(beans = 2, milk = 0, water = 5)
-}
-
-enum class CoffeeRoast {
-    DARK, LITE
 }
